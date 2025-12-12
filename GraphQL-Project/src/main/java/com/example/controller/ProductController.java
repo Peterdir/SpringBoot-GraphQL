ba12c21662870f4dacfdfa0a609fcd9df96cb766
Lam Khanh Duy <23110084@student.hcmute.edu.vn>
2025-12-12 09:24:03 +0700

    Update GraphQL

--- GraphQL-Project/src/main/java/com/example/controller/ProductController.java
+++ GraphQL-Project/src/main/java/com/example/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.example.entity.Product;
 import com.example.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +16,38 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 	private final ProductService productService;
 	
+	@QueryMapping
+	public List<Product> products(){
+		return productService.getAllProducts();
+	}
 	
+	@QueryMapping
+	public List<Product> productsByPriceAsc(){
+		return productService.getProductsByPriceAsc();
+	}
+	
+	@QueryMapping
+	public List<Product> productsByCategoryId(Long categoryId){
+		return productService.getProductsByCategoryId(categoryId);
+	}
+	
+	@QueryMapping
+	public List<Product> productsByProductId(Long productId){
+		return productService.getProductById(productId);
+	}
+	
+	@MutationMapping
+	public List<Product> createProduct(Product product){
+		return productService.createProduct(product);
+	}
+	
+	@MutationMapping
+	public List<Product> updateProduct(Long productId, Product product){
+		return productService.updateProduct(productId, product);
+	}
+	
+	@MutationMapping
+	public Boolean deleteProduct(Long productId){
+		return productService.deleteProduct(productId);
+	}
 }
